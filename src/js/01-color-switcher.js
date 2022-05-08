03720e0a004eaca566fe6a8f0747a3b675cd2794
VolodymyrTrush <vmtrush@gmail.com>
2022-05-08 20:00:45 +0300

    HW9
    Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,27 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+}
+
+const INTERVAL_CHANGE = 1000;
+let interval = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onBtnStart() {
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_CHANGE);
+  refs.btnStart.disabled = true;
+}
+
+function onBtnStop() {
+  clearInterval(interval);
+  refs.btnStart.disabled = false;
+}
+
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+
